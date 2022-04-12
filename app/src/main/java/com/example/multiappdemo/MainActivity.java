5de0bbe6aeaeeb8c5d376f22c6dae2c1707f9672
b21ricni <b21ricni@student.his.se>
2022-04-12 13:54:21 +0200

    added logging of MainActivity Lifecycle state

--- app/src/main/java/com/example/multiappdemo/MainActivity.java
+++ app/src/main/java/com/example/multiappdemo/MainActivity.java
@@ -8,9 +8,33 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("==>","Activity PostResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("==>","Activity STARTO");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("==>","Activity STAPH");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("==>","Activity Destroyed");
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("==>","WAOW")
+        Log.d("==>","WAOW");
     }
 }
